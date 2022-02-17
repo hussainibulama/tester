747486cc5f9cf752ca0cbddefd8fa42b5a6bb3fc
hussainibulama <43305254+hussainibulama@users.noreply.github.com>
2022-02-17 23:19:30 +0100

    Aask

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /var/www/admin-frontend
 npm run build
-pm2 serve build/ 3030 --name "FrontTester" --spa
+sudo pm2 restart FrontTester
+#pm2 serve build/ 3030 --name "FrontTester" --spa
