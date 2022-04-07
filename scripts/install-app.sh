3b0e1b660016fe94237e75549dae7af169454ffb
hussainibulama <43305254+hussainibulama@users.noreply.github.com>
2022-04-07 23:48:23 +0100

    ask

--- scripts/install-app.sh
+++ scripts/install-app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #rm -rf /var/www/admin-frontend
+#rm -rf node_modules public script src  build
+
 cd /var/www/admin-frontend
-rm -rf node_modules public script src  build
 npm run build
